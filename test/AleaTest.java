fe52db6c04a5a19c67e3f9c3e66ff870a08ee6fe
sakatiamy <sakatiamy@Sakatiamy-Mint-Dell>
2014-04-28 14:44:21 +0200

    testDistribution Alea -> Fonctionne correctement

--- test/AleaTest.java
+++ test/AleaTest.java
@@ -6,6 +6,7 @@
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -36,10 +37,36 @@ public class AleaTest {
     public void tearDown() {
     }
 
-    @Test
+    //@Test
     public void testBernouilli() {
         for (double i = 0; i < 1; i = i + 0.001) {
             System.out.println("" + i + " : " + Alea.bernouilli(i));
         }
     }
+
+    @Test
+    /**
+     * D'après les resultats obtenus, on peut affirmer que l'alea mis en place est correct.
+     */
+    public void testDistribution() {
+        double[] matrix;
+        int size = 100000;
+        int taille = 8;
+        double[] somme = new double[taille];
+        for (int i = 0; i < taille; i++) {
+            somme[i] = 0;
+        }
+        for (int i = 0; i < size; i++) {
+            matrix = Alea.createRandomDistribution(taille, 1000);
+            for (int j = 0; j < taille; j++) {
+                somme[j] = somme[j] + matrix[j];
+            }
+        }
+        for (int i = 0; i < taille; i++) {
+            double moyenne = somme[i] / size;
+            System.out.println("En moyenne, " + i + " : " + moyenne);
+            Assert.assertEquals(1./taille, moyenne, 0.02);
+        }
+    }
+
 }
